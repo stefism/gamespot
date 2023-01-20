ca979a01488ad5bd87c5d82defdf8af94a0b560a
Stefan Markov <stef4otm@gmail.com>
2023-01-20 16:24:45 +0200

    Make view add article

--- src/components/User/AdminArticles.vue
+++ src/components/User/AdminArticles.vue
@@ -1,13 +1,95 @@
 <template>
   <DashboardTitle title="Articles" />
+  <div v-if="adminArticles">
+    <a-table
+      :columns="columns"
+      :pagination="false"
+      :data-source="adminArticles"
+      :rowKey="(record) => record.id"
+    >
+      <template #title>
+        <router-link :to="{ name: 'admin_add' }">
+          <button class="btn btn-secondary">Add article</button>
+        </router-link>
+      </template>
+
+      <template #game="{ record }">
+        <router-link :to="{ name: 'articleInfo', params: { id: record.id } }">
+          {{ record.game }}
+        </router-link>
+      </template>
+
+      <template #owner="{ record }">
+        <span>{{ record.owner.firstName }} {{ record.owner.lastName }}</span>
+      </template>
+
+      <template #time="{ record }">
+        <span>{{ record.timestamp.toDate().toDateString() }}</span>
+      </template>
+
+      <template #delete="{ record }">
+        <a-popconfirm
+          title="Are you sure?"
+          okText="Yes"
+          cancelText="No"
+          @confirm="removeArticleById(record.id)"
+        >
+          <button class="btn btn-danger btn-sm">Delete article</button>
+        </a-popconfirm>
+      </template>
+    </a-table>
+  </div>
 </template>
 
 <script>
 import DashboardTitle from "@/components/DashboardTitle.vue";
+import { mapActions } from "vuex";
 
 export default {
   components: { DashboardTitle },
+  data() {
+    return {
+      columns,
+    };
+  },
+  computed: {
+    adminArticles() {
+      return this.$store.getters["articles/showAdminArticles"]; //Когато имаме само един гетър, не е необхидимо да правим ..mapGetters
+    },
+  },
+  mounted() {
+    this.getAdminArticles({ limit: 3 });
+  },
+  methods: {
+    ...mapActions("articles", ["getAdminArticles"]),
+    removeArticleById(articleId) {
+      console.log(articleId);
+    },
+  },
 };
+
+const columns = [
+  {
+    title: "Game",
+    slots: { customRender: "game" },
+  },
+  {
+    title: "Owner",
+    slots: { customRender: "owner" },
+  },
+  {
+    title: "Rating",
+    dataIndex: "rating",
+  },
+  {
+    title: "Created at",
+    slots: { customRender: "time" },
+  },
+  {
+    title: "",
+    slots: { customRender: "delete" },
+  },
+];
 </script>
 
 <style></style>
--- src/store/articles.js
+++ src/store/articles.js
@@ -1,4 +1,16 @@
-import { doc, setDoc, collection, serverTimestamp } from "firebase/firestore";
+/* eslint-disable */
+
+import {
+  doc,
+  setDoc,
+  getDocs,
+  collection,
+  serverTimestamp,
+  query,
+  limit,
+  orderBy,
+  startAfter,
+} from "firebase/firestore";
 import { successMessage, errorMessage } from "@/tools/vuex";
 import { db } from "@/firebase";
 import router from "@/routes";
@@ -8,7 +20,26 @@ let articlesCollection = collection(db, "articles"); //Създаваме нов
 const articlesModule = {
   namespaced: true,
   state() {
-    return {};
+    return {
+      adminArticles: [],
+      lastVisibleArticle: null,
+    };
+  },
+  getters: {
+    showAdminArticles(state) {
+      return state.adminArticles;
+    },
+    getLastVisibleArticle(state) {
+      return state.lastVisibleArticle;
+    },
+  },
+  mutations: {
+    setAdminArticles(state, articles) {
+      state.adminArticles = articles;
+    },
+    setLastVisibleArticle(state, payload) {
+      state.lastVisibleArticle = payload;
+    },
   },
   actions: {
     async addArticle({ commit, rootGetters }, payload) {
@@ -31,6 +62,27 @@ const articlesModule = {
 
         router.push({ name: "admin_articles" });
         successMessage(commit, "Article added successfully");
+      } catch (error) {
+        errorMessage(commit, error);
+      }
+    },
+    async getAdminArticles({ commit }, payload) {
+      try {
+        const currQuery = query(
+          articlesCollection,
+          orderBy("timestamp", "desc"),
+          limit(payload.limit)
+        );
+        const querySnapshot = await getDocs(currQuery);
+
+        const lastVisible = querySnapshot.docs[querySnapshot.docs.length - 1];
+        const articles = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        commit("setAdminArticles", articles);
+        commit("setLastVisibleArticle", lastVisible);
       } catch (error) {
         errorMessage(commit, error);
       }
